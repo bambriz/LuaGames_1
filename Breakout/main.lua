c1761652b79bfcf25adadedce45dede308a865e7
bambriz <bambriz@ucsc.edu>
2018-02-10 11:36:28 -0800

    update
     adding table for bricks

--- Breakout/main.lua
+++ Breakout/main.lua
@@ -25,15 +25,24 @@ brick.position_y = 100
 brick.width = 50
 brick.height = 30
 
+--Bricks
+local bricks = {}
+bricks.current_level_bricks = {}
 
-function love.load()
+--bricks functions
+function bricks.new_brick( position_x, position_y, width, height)
+  return({ position_x = position_x,
+             position_y = position_y,
+             width = width or bricks.brick_width,           --(*1)
+             height = height or bricks.brick_height })
 end
 
-function love.update(dt)
-  --update ball position
+--update functions
+function ball.update(dt)
   ball.position_x = ball.position_x + ball.speed_x * dt --(*1)
   ball.position_y = ball.position_y + ball.speed_y * dt
-  --platform controls
+end
+function platform.update(dt)
   if love.keyboard.isDown('right') then 
     platform.position_x = platform.position_x+(platform.speed_x*dt)
   end
@@ -42,25 +51,53 @@ function love.update(dt)
   end
 end
 
-function love.draw()
-  local segments_in_circle = 16
-  --draw ball
-  love.graphics.circle('line',
+-- draw functions
+function ball.draw()
+   local segments_in_circle = 16
+    love.graphics.circle('line',
                         ball.position_x,
                         ball.position_y,
                         ball.radius,
                         segments_in_circle
                       )
-  -- draw platform
-  love.graphics.rectangle( 'line',
+end
+
+function platform.draw()
+  
+    love.graphics.rectangle( 'line',
                             platform.position_x,
                             platform.position_y,
                             platform.width,
                             platform.height)
+end
+
+--Main functions
+function love.load()
+end
+
+function love.update(dt)
+  --update ball position
+  ball.update(dt)
+  --platform controls
+  platform.update(dt)
+  
+end
+
+function love.draw()
+ 
+  --draw ball
+  ball.draw()
+  -- draw platform
+  platform.draw()
   -- draw brick
   love.graphics.rectangle( 'line',
                             brick.position_x,
                             brick.position_y,
                             brick.width,
                             brick.height)
+end
+
+--what to do when game quits
+function love.quit()
+  print("Thank you for playing.")
 end
