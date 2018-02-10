a2539672c692aa18ed092c8123df689e687b6d6c
bambriz <bambriz@ucsc.edu>
2018-02-10 03:39:56 -0800

    Objects and Controls
    Added code for game objects, and the controls for the platform

--- Breakout/main.lua
+++ Breakout/main.lua
@@ -1,8 +1,38 @@
---(*1) 
+--Bryan Ambriz
+
+--Game objects
+
+--Ball
+local ball = {}
+ball.position_x = 300
+ball.position_y = 300
+ball.speed_x = 300
+ball.speed_y = 300
+
+--Platform
+local platform = {}
+platform.position_x = 500
+platform.position_y = 500
+platform.speed_x = 300
+
+--Brick
+local brick = {}
+brick.position_x = 100
+brick.position_y = 100
+
+
 function love.load()
 end
 
 function love.update(dt)
+  ball.position_x = ball.position_x + ball.speed_x * dt --(*1)
+  ball.position_y = ball.position_y + ball.speed_y * dt
+  if love.keyboard.isDown('right') then 
+    platform.position_x = platform.position_x+(platform.speed_x*dt)
+  end
+  if love.keyboard.isDown('left') then 
+    platform.position_x = platform.position_x-(platform.speed_x*dt)
+  end
 end
 
 function love.draw()
